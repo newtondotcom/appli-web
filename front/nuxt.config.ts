ffbbbfb308e5fd82e18aa522f7a7486771af25a6
newtondotcom <asphalt9fr@gmail.com>
2024-04-23 11:07:43 +0200

    shadcn installed

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -1,4 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: false },
+  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt"],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  },
+  ssr : false,
+  security: {
+    corsHandler: {
+      origin: '*',
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      allowedHeaders: '*',
+      exposeHeaders: '*',
+    },
+  },
 })
