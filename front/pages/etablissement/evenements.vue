864efb1251376ba4d2253fca39efec95dfd59c21
newtondotcom <asphalt9fr@gmail.com>
2024-05-09 13:08:58 +0200

    feat : page qui liste les évènements

--- front/pages/etablissement/evenements.vue
+++ front/pages/etablissement/evenements.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
 const route = useRoute()
-const id = route.params.id
+const id = route.params.id as number
 
+const nom = "Découverte IA";
+const description = "Venez découvrir l'intelligence artificielle avec nos collaborateurs durant ce stage de 3 semaines.";
+const creneau : string = new Date().toLocaleDateString() + " - " + new Date().toLocaleTimeString();
+const nom_etablissement = "Airbus";
+const id_etablissement = 0;
+const note_etablissement = 4.5;
+const tags = ["IA", "Stage", "Airbus"];
+const evenement : EvenementEtablissement = {id, nom, description, creneau, nom_etablissement, id_etablissement, note_etablissement, tags};
+  
 </script>
 
 <template>
-  <div>
-    <h1>Événement</h1>
-  </div>
+<Titre title="Evènements" subtitle="Voici les évènements proposés par votre établissement" />
+<EvenementCarte :evenement="evenement" :key="evenement.id" />
 </template>
