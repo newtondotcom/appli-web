6216c0a50bf499dab4361bf97a0c99e10b859857
newtondotcom <asphalt9fr@gmail.com>
2024-05-09 13:36:42 +0200

    feat : opacité si l'evenement est passé + changement date

--- front/components/evenement/carte.vue
+++ front/components/evenement/carte.vue
@@ -9,14 +9,15 @@ import { Calendar, Star } from 'lucide-vue-next';
 </script>
 
 <template>
-  <div class="flex space-y-4 justify-center align-middle content-center mx-auto">
+  <div class="flex space-y-4 justify-center align-middle content-center mx-auto"
+    :class="{ 'opacity-50': new Date(evenement.creneau) < new Date() }">
     <Card class="w-[70vw]">
       <CardHeader class="flex flex-row items-center justify-between space-y-0 pb-2">
         <CardTitle class="text-2xl font-medium">
           {{ evenement.nom }}
         </CardTitle>
         <div class="flex flex-row">
-          <Calendar class="mr-2" /> {{ evenement.creneau }}
+          <Calendar class="mr-2" /> {{ new Date(evenement.creneau).toLocaleDateString() }} - {{ new Date(evenement.creneau).toLocaleTimeString() }}
         </div>
       </CardHeader>
       <CardContent>
@@ -28,7 +29,9 @@ import { Calendar, Star } from 'lucide-vue-next';
             <div class="flex justify-between">
               <div>
                 <Badge class="mr-2">{{ evenement.nom_etablissement }} </Badge>
-                <Badge variant="secondary">{{ evenement.note_etablissement }} / 5 <Star class="ml-1" size="14"/></Badge>
+                <Badge variant="secondary">{{ evenement.note_etablissement }} / 5
+                  <Star class="ml-1" size="14" />
+                </Badge>
               </div>
               <div class="flex flex-row mt-2">
                 <Badge variant="primary" class="mr-2" v-for="tag in evenement.tags" :key="tag">{{ tag }}</Badge>
