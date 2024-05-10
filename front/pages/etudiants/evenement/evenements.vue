4487ba259d38486ff2c1230b7bfbacf3130dce57
jaubry2 <120647406+jaubry2@users.noreply.github.com>
2024-05-10 18:34:50 +0200

    feat =

--- front/pages/etudiants/evenement/evenements.vue
+++ front/pages/etudiants/evenement/evenements.vue
@@ -1,56 +1,66 @@
 <script setup lang="ts">
-interface Evenement {
+interface EvenementEtablissement {
   id: number;
-  title: string;
   nom: string;
-  date: string;
-  domaine: string;
+  description: string;
+  creneau: Date;
+  nom_etablissement: string;
+  id_etablissement: number;
+  note_etablissement: number;
+  tags: string[];
 }
-const evenements: Evenement[] = [
+
+const evenements: EvenementEtablissement[] = [
+  {
+    id: 0,
+    nom: "Découverte IA",
+    description:
+      "Venez découvrir l'intelligence artificielle avec nos collaborateurs durant ce stage de 3 heures.",
+    creneau: new Date(),
+    nom_etablissement: "Airbus",
+    id_etablissement: 0,
+    note_etablissement: 4.5,
+    tags: ["IA", "Stage", "Airbus"],
+  },
   {
     id: 1,
-    title: "Conférence sur le développement durable",
-    nom: "Hall Of Change",
-    date: "2023/04/15",
-    domaine: "Environnement",
+    nom: "Formation Python",
+    description:
+      "Apprenez les bases du langage de programmation Python avec nos experts.",
+    creneau: new Date(),
+    nom_etablissement: "Microsoft",
+    id_etablissement: 1,
+    note_etablissement: 4.8,
+    tags: ["Python", "Formation", "Microsoft"],
   },
   {
     id: 2,
-    title: "Atelier sur la programmation fonctionnelle",
-    nom: "Microsoft",
-    date: "2023/05/01",
-    domaine: "Informatique",
-  },
-  {
-    id: 3,
-    title: "Concert de musique classique",
-    nom: "Opéra de Toulouse",
-    date: "2023/06/10",
-    domaine: "Culture",
+    nom: "Atelier Design Thinking",
+    description:
+      "Découvrez la méthode de Design Thinking pour résoudre des problèmes complexes en équipe.",
+    creneau: new Date(),
+    nom_etablissement: "Google",
+    id_etablissement: 2,
+    note_etablissement: 4.7,
+    tags: ["Design Thinking", "Atelier", "Google"],
   },
 ];
 </script>
 
 <template>
-  <div class="relative w-full max-w-sm items-center mx-[40%]">
-    <Input
-      id="search"
-      type="text"
-      placeholder="Search..."
-      class="pl-10 w-[50%] justify-center content-center align-center"
-    />
-    <span
-      class="absolute start-0 inset-y-0 flex items-center justify-center px-2"
-    >
-      <Search class="size-6 text-muted-foreground" />
-    </span>
-  </div>
-  <div class="flex justify-center flex-wrap gap-8 p-4">
-    <EtudiantCarte
-      v-for="evenement in evenements"
-      :key="evenement.id"
-      :evenement="evenement"
-    />
+  <div class="">
+    <EtudiantSearchBar />
+    <div class="flex flex-wrap justify-center">
+      <div
+        v-for="evenement in evenements"
+        :key="evenement.id"
+        class="w-[40%] mx-4"
+      >
+        <a :href="'' + evenement.id" class="">
+          <EvenementCarte class="my-4 mx-4" :evenement="evenement" />
+        </a>
+      </div>
+    </div>
   </div>
 </template>
 
