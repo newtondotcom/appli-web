d1bebff84a4cc4e869a88c87c65eeefa8881ac46
jaubry2 <120647406+jaubry2@users.noreply.github.com>
2024-05-10 18:35:37 +0200

    feat : page qui correspond à un evenment du pdv etudiant

--- front/pages/etudiants/evenement/[id].vue
+++ front/pages/etudiants/evenement/[id].vue
@@ -1,9 +1,48 @@
+<script setup lang="ts">
+const postulants = [
+  {
+    id: 0,
+    nom: "Jean Dupont",
+    email: "jean.dupont@gmail.com",
+    numero: "06 12 34 56 78",
+    statut: "accepte",
+  },
+  {
+    id: 1,
+    nom: "Marie Dupont",
+    email: "marie.dupont@gmail.com",
+    numero: "06 12 34 56 78",
+    statut: "refuse",
+  },
+  {
+    id: 2,
+    nom: "Paul Dupont",
+    email: "paul.dupont@gmail.com",
+    numero: "06 12 34 56 78",
+    statut: "en attente",
+  },
+];
+const id = 0;
+const nom = "Découverte IA";
+const description =
+  "Venez découvrir l'intelligence artificielle avec nos collaborateurs durant ce stage de 3 heures.";
+const creneau: Date = new Date();
+const nom_etablissement = "Airbus";
+const id_etablissement = 0;
+const note_etablissement = 4.5;
+const tags = ["IA", "Stage", "Airbus"];
+const evenement: EvenementEtablissement = {
+  id,
+  nom,
+  description,
+  creneau,
+  nom_etablissement,
+  id_etablissement,
+  note_etablissement,
+  tags,
+};
+</script>
+
 <template>
-  <div>
-    <EvenementCarte />
-  </div>
+  <EvenementCarte :key="evenement.id" :evenement="evenement" />
 </template>
-
-<script></script>
-
-<style></style>
