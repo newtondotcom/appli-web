3717126686d287e2f8aaa794829a5c4e5bfb1333
jaubry2 <120647406+jaubry2@users.noreply.github.com>
2024-05-11 17:04:16 +0200

    feat : peut ajouter une lettre de motivation et sa pièce d'identité

--- front/pages/etudiants/evenement/[id].vue
+++ front/pages/etudiants/evenement/[id].vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { ref } from "vue";
+import { Loader2 } from "lucide-vue-next";
 const postulants = [
   {
     id: 0,
@@ -22,7 +24,7 @@ const postulants = [
     statut: "en attente",
   },
 ];
-const id = 0;
+const idd = 0;
 const nom = "Découverte IA";
 const description =
   "Venez découvrir l'intelligence artificielle avec nos collaborateurs durant ce stage de 3 heures.";
@@ -32,7 +34,7 @@ const id_etablissement = 0;
 const note_etablissement = 4.5;
 const tags = ["IA", "Stage", "Airbus"];
 const evenement: EvenementEtablissement = {
-  id,
+  idd,
   nom,
   description,
   creneau,
@@ -41,8 +43,80 @@ const evenement: EvenementEtablissement = {
   note_etablissement,
   tags,
 };
+const lettreDeMotiv = ref("");
+const requeteFausse = ref(false);
+const loading = ref(false);
+const fileSelected = ref(false);
+const fileName = ref("");
+const demandeEnvoyee = ref(false);
+
+async function sendDemand() {
+  console.log("sendDemand");
+  loading.value = true;
+  if (lettreDeMotiv.value === "" || !fileSelected.value) {
+    requeteFausse.value = true;
+    loading.value = false;
+    return;
+  }
+  requeteFausse.value = false;
+  loading.value = false;
+  demandeEnvoyee.value = true;
+}
+
+const handleFileChange = (event: Event) => {
+  const target = event.target as HTMLInputElement;
+  if (target.files && target.files.length > 0) {
+    fileSelected.value = true;
+    fileName.value = target.files[0].name;
+  } else {
+    fileSelected.value = false;
+    fileName.value = "";
+  }
+};
 </script>
 
 <template>
-  <EvenementCarte :key="evenement.id" :evenement="evenement" />
+  <div class="flex flex-wrap justify-center gap-10">
+    <EvenementCarte :key="evenement.id" :evenement="evenement" />
+    <div class="grid w-full max-w-sm items-center gap-14 content-center">
+      <div>
+        <Label for="lettreDeMotiv" class="place-self-center"
+          >Lettre de Motivation</Label
+        >
+        <Input
+          id="lettreDeMotiv"
+          type="text"
+          placeholder="Cette visite m'intéresse car …"
+          class="h-48"
+          v-model="lettreDeMotiv"
+          :disabled="demandeEnvoyee"
+        />
+      </div>
+      <div>
+        <Label for="picture">Papier d'identité</Label>
+        <Input
+          id="picture"
+          type="file"
+          @change="handleFileChange"
+          :disabled="demandeEnvoyee"
+        />
+      </div>
+      <Button
+        @click="sendDemand"
+        variant="secondary"
+        class="w-[20%] place-self-center"
+        :disabled="demandeEnvoyee"
+      >
+        <div v-if="loading">
+          <Loader2 class="w-4 h-4 mr-2 animate-spin" />
+          Please wait
+        </div>
+        Postuler
+      </Button>
+    </div>
+    <Alert v-if="requeteFausse" variant="destructive" class="w-[70%]">
+      <AlertTitle>Erreur</AlertTitle>
+      <AlertDescription> Vous devez remplir tous les champs </AlertDescription>
+    </Alert>
+  </div>
 </template>
