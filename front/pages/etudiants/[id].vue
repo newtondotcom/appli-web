3b10b3ca3114a370dfa72d76ac62c9124e6c4e13
jaubry2 <120647406+jaubry2@users.noreply.github.com>
2024-05-23 18:09:22 +0200

    feat : ajout de la carte d'identité sur le profil

--- front/pages/etudiants/[id].vue
+++ front/pages/etudiants/[id].vue
@@ -1,109 +1,107 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import { Loader2, TicketCheck, TicketSlash, TicketX } from "lucide-vue-next";
+import { useToast } from "@/components/ui/toast/use-toast";
+import { Settings } from "lucide-vue-next";
+import { useRoute } from "vue-router";
+const { toast } = useToast();
 
-const lettreDeMotiv = ref("");
-const requeteFausse = ref(false);
+const studentFirstName = ref("");
+const studentLastName = ref("");
+const studentClass = ref("");
+const studentEtablissement = ref("");
+const studentDescription = ref("");
 const loading = ref(false);
-const fileSelected = ref(false);
-const fileName = ref("");
-const demandeEnvoyee = ref(false);
+const isModifying = ref(false);
 
 const route = useRoute();
-
-const id = route.params.id;
-const bool = route.query.bool === "true";
-console.log(route);
-console.log(id);
-console.log(bool);
-
-const data = await $fetch(
-  `http://localhost:8080/PasserellePro/Serv?op=get_evenement_from_id&id=${id}`
-);
-const evenement = data;
-
-/* Demande déja effectué */
-const demandeEffectue = true;
-const demandeValide = ref(false);
-const demandeEnAttente = ref(true);
-const demandeRefuse = ref(false);
-if (demandeEffectue) {
-  demandeEnvoyee.value = true;
-  // Récupérer la demande du boug
+let id = -1;
+if (route.params.id) {
+  id = route.params.id as number;
 }
 
-async function sendDemand() {
-  console.log("sendDemand");
+async function saveChanges() {
   loading.value = true;
-  if (lettreDeMotiv.value === "" || !fileSelected.value) {
-    requeteFausse.value = true;
-    loading.value = false;
-    return;
-  }
-  requeteFausse.value = false;
+  toast({
+    title: "Succès",
+    description: "Les modifications ont été enregistrées avec succès",
+  });
   loading.value = false;
-  demandeEnvoyee.value = true;
 }
 
-const handleFileChange = (event: Event) => {
-  const target = event.target as HTMLInputElement;
-  if (target.files && target.files.length > 0) {
-    fileSelected.value = true;
-    fileName.value = target.files[0].name;
-  } else {
-    fileSelected.value = false;
-    fileName.value = "";
-  }
-};
+async function Modifyprofil() {
+  isModifying.value = !isModifying.value;
+}
 </script>
 
 <template>
-  <div class="flex flex-wrap justify-center gap-10">
-    <EvenementCarte :key="evenement.id" :evenement="evenement" />
-    <div class="grid w-full max-w-sm items-center gap-14 content-center">
-      <div v-if="demandeEnvoyee" class="flex justify-center">
-        <TicketCheck color="#3e9392" class="w-20 h-20" />
-        <TicketSlash color="#d95000" class="w-20 h-20" />
-        <TicketX color="#b51a00" class="w-20 h-20" />
-      </div>
-      <div>
-        <Label for="lettreDeMotiv" class="place-self-center"
-          >Lettre de Motivation</Label
-        >
-        <Input
-          id="lettreDeMotiv"
-          type="text"
-          placeholder="Cette visite m'intéresse car …"
-          class="h-48"
-          v-model="lettreDeMotiv"
-          :disabled="demandeEnvoyee"
-        />
-      </div>
-      <div>
-        <Label for="picture">Papier d'identité</Label>
-        <Input
-          id="picture"
-          type="file"
-          @change="handleFileChange"
-          :disabled="demandeEnvoyee"
-        />
-      </div>
+  <div class="flex flex-col">
+    <div class="absolute right-4">
       <Button
-        @click="sendDemand"
-        variant="secondary"
-        class="w-[20%] place-self-center"
-        :disabled="demandeEnvoyee"
+        @click="Modifyprofil"
+        :variant="isModifying ? undefined : 'secondary'"
+        size="icon"
       >
-        <div v-if="loading">
-          <Loader2 class="w-4 h-4 mr-2 animate-spin" />
-          Please wait
-        </div>
-        Postuler
+        <Settings class="w-4 h-4" />
       </Button>
     </div>
-    <Alert v-if="requeteFausse" variant="destructive" class="w-[70%]">
-      <AlertTitle>Erreur</AlertTitle>
-      <AlertDescription> Vous devez remplir tous les champs </AlertDescription>
-    </Alert>
+    <Titre
+      title="Profil"
+      subtitle="Voici les informations que voit les entreprises."
+      class="place-self-center"
+    />
+    <div class="grid gap-4 px-4">
+      <div class="grid gap-3">
+        <Label for="studentLastName">Nom</Label>
+        <Input
+          id="studentLastName"
+          type="text"
+          v-model="studentLastName"
+          :disabled="!isModifying"
+        />
+      </div>
+      <div class="grid gap-3">
+        <Label for="studentFirstName">Prénom</Label>
+        <Input
+          id="studentFirstName"
+          type="text"
+          v-model="studentFirstName"
+          :disabled="!isModifying"
+        />
+      </div>
+      <div class="grid gap-3">
+        <Label for="studentClass">Classe</Label>
+        <Input
+          id="studentClass"
+          type="text"
+          v-model="studentClass"
+          :disabled="!isModifying"
+        />
+      </div>
+      <div class="grid gap-3">
+        <Label for="studentEstalishment">Établissement</Label>
+        <Input
+          id="studentEstalishment"
+          type="text"
+          v-model="studentEtablissement"
+          :disabled="!isModifying"
+        />
+      </div>
+      <div class="grid gap-3">
+        <Label for="studentDescription">Établissement</Label>
+        <Textarea
+          id="studentDescription"
+          v-model="studentDescription"
+          :disabled="!isModifying"
+        />
+        <Button
+          @click="saveChanges"
+          :disabled="isModifying ? undefined : 'secondary'"
+        >
+          <div v-if="!loading" class="flex items-center space-x-2">
+            Enregistrer les modifications
+          </div>
+        </Button>
+      </div>
+    </div>
   </div>
 </template>
