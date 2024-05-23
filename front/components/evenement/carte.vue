6d440db05ebfd02d7b2979030063b148fb49966b
jaubry2 <120647406+jaubry2@users.noreply.github.com>
2024-05-23 17:54:32 +0200

    fix : modif du nom en titre

--- front/components/evenement/carte.vue
+++ front/components/evenement/carte.vue
@@ -5,19 +5,25 @@ defineProps({
     required: true,
   },
 });
-import { Calendar, Star} from 'lucide-vue-next';
+import { Calendar, Star } from "lucide-vue-next";
 </script>
 
 <template>
-  <div class="flex space-y-4 justify-center align-middle content-center mx-auto"
-    :class="{ 'opacity-50': new Date(evenement.creneau) < new Date() }">
+  <div
+    class="flex space-y-4 justify-center align-middle content-center mx-auto"
+    :class="{ 'opacity-50': new Date(evenement.creneau) < new Date() }"
+  >
     <Card class="w-[70vw]">
-      <CardHeader class="flex flex-row items-center justify-between space-y-0 pb-2">
+      <CardHeader
+        class="flex flex-row items-center justify-between space-y-0 pb-2"
+      >
         <CardTitle class="text-2xl font-medium">
-          {{ evenement.nom }}
+          {{ evenement.titre }}
         </CardTitle>
         <div class="flex flex-row">
-          <Calendar class="mr-2" /> {{ new Date(evenement.creneau).toLocaleDateString() }} - {{ new Date(evenement.creneau).toLocaleTimeString() }}
+          <Calendar class="mr-2" />
+          {{ new Date(evenement.creneau).toLocaleDateString() }} -
+          {{ new Date(evenement.creneau).toLocaleTimeString() }}
         </div>
       </CardHeader>
       <CardContent>
@@ -29,12 +35,15 @@ import { Calendar, Star} from 'lucide-vue-next';
             <div class="flex justify-between">
               <div>
                 <Badge class="mr-2">{{ evenement.nom_etablissement }} </Badge>
-                <Badge variant="secondary">{{ evenement.note_etablissement }} / 5
-                  <Star class="ml-1" :size=14 />
+                <Badge variant="secondary"
+                  >{{ evenement.note_etablissement }} / 5
+                  <Star class="ml-1" :size="14" />
                 </Badge>
               </div>
               <div class="flex flex-row mt-2">
-                <Badge class="mr-2" v-for="tag in evenement.tags" :key="tag">{{ tag }}</Badge>
+                <Badge class="mr-2" v-for="tag in evenement.tags" :key="tag">{{
+                  tag
+                }}</Badge>
               </div>
             </div>
           </div>
